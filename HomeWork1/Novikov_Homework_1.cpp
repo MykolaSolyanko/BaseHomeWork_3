224c29090f3340d76f45952fc06215ed12b4b8c9
Vadim Novikov <vadimnovikovrockstar@gmail.com>
2020-05-02 14:20:47 +0300

    Changed if politics and resolve other problems

--- HomeWork1/Novikov_Homework_1.cpp
+++ HomeWork1/Novikov_Homework_1.cpp
@@ -9,21 +9,21 @@ int main() {
 	const int TWO = 2;
 
 
-	int a;
+	int a{};
 	std::cout << R"(Enter 'a' koef )";
 	if (!(std::cin >> a)) {
 		std::cout << "Incorrect range\n";
 		return 0;
 	}
 
-	int b;
+	int b{};
 	std::cout << R"(Enter 'b' koef )";
 	if (!(std::cin >> b)) {
 		std::cout << "Incorrect range\n";
 		return 0;
 	}
 
-	int c;
+	int c{};
 	std::cout << R"(Enter 'c' koef )";
 	if (!(std::cin >> c)) {
 		std::cout << "Incorrect range\n";
@@ -43,6 +43,13 @@ int main() {
 		std::cout << "x2 = " << x2 << std::endl;
 		return 0;
 	}
+
+	// c = 0
+	if (a == 0 && b == 0) {
+		std::cout << "This is not an equation" << std::endl;
+		return 0;
+	}
+
 	// ax^2+c=0
 	if (b == 0 && a != 0 && c != 0) {
 		if ( fc / a > 0) {
@@ -61,13 +68,7 @@ int main() {
 	if (a == 0 && b != 0) {
 		std::cout << "x = " << fc / b;
 		return 0;
-	}
-
-	// c = 0
-	if (a == 0 && b == 0) {
-		std::cout << "This is not an equation" << std::endl;
-		return 0;
-	}
+	}	
 
 	// ax^2+bx=0
 	if (c == 0 && a != 0) {
