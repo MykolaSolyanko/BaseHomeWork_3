f32a2ae61c5d661c1de9e68914db1bb122d43697
Vadim Novikov <vadimnovikovrockstar@gmail.com>
2020-05-02 14:17:48 +0300

    Changed if politics

--- HomeWork1/Novikov_Homework_1.cpp
+++ HomeWork1/Novikov_Homework_1.cpp
@@ -34,16 +34,23 @@ int main() {
 	//roots of equation
 	float x1; 
 	float x2;
+	float fc = static_cast<float>(-c);
+	float fb = static_cast<float>(-b);
 	// ax^2 = 0
 	if (b == 0 && c == 0) {
 		x1 = x2 = 0;
+		std::cout << "x1 = " << x1 << std::endl;
+		std::cout << "x2 = " << x2 << std::endl;
 		return 0;
 	}
 	// ax^2+c=0
 	if (b == 0 && a != 0 && c != 0) {
-		if (static_cast<float>(-c) / a > 0) {
-			x1 = sqrt(static_cast<float>(-c) / a);
-			x2 = -sqrt(static_cast<float>(-c) / a);
+		if ( fc / a > 0) {
+			x1 = sqrt(fc / a);
+			x2 = -sqrt(fc / a);
+			std::cout << "x1 = " << x1 << std::endl;
+			std::cout << "x2 = " << x2 << std::endl;
+			return 0;
 		}
 		else {
 			std::cout << "There's no real solutions" << std::endl;
@@ -52,7 +59,7 @@ int main() {
 	
 		// bx+c=0
 	if (a == 0 && b != 0) {
-		std::cout << "x = " << -c / static_cast<float>(b);
+		std::cout << "x = " << fc / b;
 		return 0;
 	}
 
@@ -65,7 +72,10 @@ int main() {
 	// ax^2+bx=0
 	if (c == 0 && a != 0) {
 		x1 = 0;
-		x2 = static_cast<float>(-b) / a;
+		x2 = fb / a;
+		std::cout << "x1 = " << x1 << std::endl;
+		std::cout << "x2 = " << x2 << std::endl;
+		return 0;
 	}
 
 	// ax^2+bx+c = 0
@@ -76,13 +86,19 @@ int main() {
 			return 0;
 		}
 		if (D == 0)
-			x1 = x2 = -b / static_cast<float>(TWO * a);
+		{
+			x1 = x2 = fb / (TWO * a);
+			std::cout << "x1 = " << x1 << std::endl;
+			std::cout << "x2 = " << x2 << std::endl;
+			return 0;
+		}
 		else {
 			x1 = (-b + sqrt(D)) / (TWO * a);
 			x2 = (-b - sqrt(D)) / (TWO * a);
+			std::cout << "x1 = " << x1 << std::endl;
+			std::cout << "x2 = " << x2 << std::endl;
+			return 0;
 		}
 	}
-	std::cout << "x1 = " << x1 << std::endl;
-	std::cout << "x2 = " << x2 << std::endl;
-	return 0;
+	
 }
