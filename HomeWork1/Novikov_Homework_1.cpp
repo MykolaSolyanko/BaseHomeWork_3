87768869bea0fa60849f6b9ae310261683d73f73
Vadim Novikov <vadimnovikovrockstar@gmail.com>
2020-05-02 15:40:27 +0300

    another errors fixed

--- HomeWork1/Novikov_Homework_1.cpp
+++ HomeWork1/Novikov_Homework_1.cpp
@@ -32,7 +32,7 @@ int main() {
 
 
 	//roots of equation
-	float x1; 
+	float x1;
 	float x2;
 	float fc = static_cast<float>(-c);
 	float fb = static_cast<float>(-b);
@@ -52,7 +52,7 @@ int main() {
 
 	// ax^2+c=0
 	if (b == 0 && a != 0 && c != 0) {
-		if ( fc / a > 0) {
+		if (fc / a > 0) {
 			x1 = sqrt(fc / a);
 			x2 = -sqrt(fc / a);
 			std::cout << "x1 = " << x1 << std::endl;
@@ -63,43 +63,44 @@ int main() {
 			std::cout << "There's no real solutions" << std::endl;
 			return 0;
 		}
-	
+	}
+
 		// bx+c=0
-	if (a == 0 && b != 0) {
-		std::cout << "x = " << fc / b;
-		return 0;
-	}	
-
-	// ax^2+bx=0
-	if (c == 0 && a != 0) {
-		x1 = 0;
-		x2 = fb / a;
-		std::cout << "x1 = " << x1 << std::endl;
-		std::cout << "x2 = " << x2 << std::endl;
-		return 0;
-	}
-
-	// ax^2+bx+c = 0
-	if (a != 0 && b != 0 && c != 0) {
-		long double D{ b * b - FOUR * a * c };
-		if (D < 0) {
-			std::cout << "There's no real solutions" << std::endl;
+		if (a == 0 && b != 0) {
+			std::cout << "x = " << fc / b;
 			return 0;
 		}
-		if (D == 0)
-		{
-			x1 = x2 = fb / (TWO * a);
+
+		// ax^2+bx=0
+		if (c == 0 && a != 0) {
+			x1 = 0;
+			x2 = fb / a;
 			std::cout << "x1 = " << x1 << std::endl;
 			std::cout << "x2 = " << x2 << std::endl;
 			return 0;
 		}
-		else {
-			x1 = (-b + sqrt(D)) / (TWO * a);
-			x2 = (-b - sqrt(D)) / (TWO * a);
-			std::cout << "x1 = " << x1 << std::endl;
-			std::cout << "x2 = " << x2 << std::endl;
-			return 0;
+
+		// ax^2+bx+c = 0
+		if (a != 0 && b != 0 && c != 0) {
+			long double D = b * b - FOUR * a * c;
+			if (D < 0) {
+				std::cout << "There's no real solutions" << std::endl;
+				return 0;
+			}
+			if (D == 0)
+			{
+				x1 = x2 = fb / (TWO * a);
+				std::cout << "x1 = " << x1 << std::endl;
+				std::cout << "x2 = " << x2 << std::endl;
+				return 0;
+			}
+			else {
+				x1 = (-b + sqrt(D)) / (TWO * a);
+				x2 = (-b - sqrt(D)) / (TWO * a);
+				std::cout << "x1 = " << x1 << std::endl;
+				std::cout << "x2 = " << x2 << std::endl;
+				return 0;
+			}
 		}
-	}
-	
+
 }
