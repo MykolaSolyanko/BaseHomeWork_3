33ba349bc57dd71905689078516823249552be51
Vadim Novikov <vadimnovikovrockstar@gmail.com>
2020-05-02 09:28:14 +0300

    Cleaning out my mess

---
+++ HomeWork1/Novikov_Homework_1.cpp
@@ -0,0 +1,88 @@
+#include <cmath>
+#include <iostream>
+#include <limits>
+
+int main() {
+
+	//consts
+	const int FOUR = 4;
+	const int TWO = 2;
+
+
+	int a;
+	std::cout << R"(Enter 'a' koef )";
+	if (!(std::cin >> a)) {
+		std::cout << "Incorrect range\n";
+		return 0;
+	}
+
+	int b;
+	std::cout << R"(Enter 'b' koef )";
+	if (!(std::cin >> b)) {
+		std::cout << "Incorrect range\n";
+		return 0;
+	}
+
+	int c;
+	std::cout << R"(Enter 'c' koef )";
+	if (!(std::cin >> c)) {
+		std::cout << "Incorrect range\n";
+		return 0;
+	}
+
+
+	//roots of equation
+	float x1; 
+	float x2;
+	// ax^2 = 0
+	if (b == 0 && c == 0) {
+		x1 = x2 = 0;
+		return 0;
+	}
+	// ax^2+c=0
+	if (b == 0 && a != 0 && c != 0) {
+		if (static_cast<float>(-c) / a > 0) {
+			x1 = sqrt(static_cast<float>(-c) / a);
+			x2 = -sqrt(static_cast<float>(-c) / a);
+		}
+		else {
+			std::cout << "There's no real solutions" << std::endl;
+			return 0;
+		}
+	
+		// bx+c=0
+	if (a == 0 && b != 0) {
+		std::cout << "x = " << -c / static_cast<float>(b);
+		return 0;
+	}
+
+	// c = 0
+	if (a == 0 && b == 0) {
+		std::cout << "This is not an equation" << std::endl;
+		return 0;
+	}
+
+	// ax^2+bx=0
+	if (c == 0 && a != 0) {
+		x1 = 0;
+		x2 = static_cast<float>(-b) / a;
+	}
+
+	// ax^2+bx+c = 0
+	if (a != 0 && b != 0 && c != 0) {
+		long double D{ b * b - FOUR * a * c };
+		if (D < 0) {
+			std::cout << "There's no real solutions" << std::endl;
+			return 0;
+		}
+		if (D == 0)
+			x1 = x2 = -b / static_cast<float>(TWO * a);
+		else {
+			x1 = (-b + sqrt(D)) / (TWO * a);
+			x2 = (-b - sqrt(D)) / (TWO * a);
+		}
+	}
+	std::cout << "x1 = " << x1 << std::endl;
+	std::cout << "x2 = " << x2 << std::endl;
+	return 0;
+}
