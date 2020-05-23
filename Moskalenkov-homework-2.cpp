4c33321edbcc862cd1db7d883fbbadc6617aacd7
Moskalenkov <64351851+Moskalenkov@users.noreply.github.com>
2020-05-23 20:49:11 +0300

    Update Moskalenkov-homework-2.cpp

--- Moskalenkov-homework-2.cpp
+++ Moskalenkov-homework-2.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 
 int main() {
+    enum {
+        CalculateTheSumAndArithmeticMeanOfDigitsInTheNumber = 1,
+        LuckyTticket,
+        InverseNumbers,
+        SumOfOddElements,
+        BestDivisor
+    };
     std::cout << "What task do you want to solve ?" << std::endl;
 
-    int setNumber(0);
+    int setNumber{};
 
     while (setNumber < 1 || setNumber > 5) {
         std::cout
-            << "1: Calculate the sum and arithmetic mean of digits in the number"
+            << CalculateTheSumAndArithmeticMeanOfDigitsInTheNumber
+            << " Calculate the sum and arithmetic mean of digits in the number"
             << std::endl;
-        std::cout << "2: Lucky ticket " << std::endl;
-        std::cout << "3: Inverse numbers " << std::endl;
-        std::cout << "4: Sum of odd elements " << std::endl;
-        std::cout << "5: Best divisor " << std::endl;
+        std::cout << LuckyTticket << " Lucky ticket " << std::endl;
+        std::cout << InverseNumbers << " Inverse numbers " << std::endl;
+        std::cout << SumOfOddElements << " Sum of odd elements " << std::endl;
+        std::cout << BestDivisor << " Best divisor " << std::endl;
         std::cin >> setNumber;
     }
-    if (setNumber == 1) {
-        int number(0);
-        int sum(0);
-        int i(0);
+    if (setNumber == CalculateTheSumAndArithmeticMeanOfDigitsInTheNumber) {
+        int number{};
+        int sum{};
+        int i{};
         std::cout << "Enter number: " << std::endl;
         std::cin >> number;
         while (number != 0) {
@@ -32,10 +40,10 @@ int main() {
         std::cout << result << std::endl;
     }
 
-    else if (setNumber == 2) {
-        int number(0);
-        int sum1(0);
-        int sum2(0);
+    else if (setNumber == LuckyTticket) {
+        int number{};
+        int sum1{};
+        int sum2{};
         constexpr int kRangeMax{ 1000000 };
         constexpr int kRangeMin{ 100000 };
 
@@ -45,18 +53,17 @@ int main() {
             int number1 = number % 1000;
             while (number1 != 0) {
                 int a = number1 % 10;
-                sum1 = sum1 + a;
+                sum1 += a;
                 number1 = (number1 - a) / 10;
             }
             number = (number - (number % 1000)) / 1000;
             while (number != 0) {
                 int a = number % 10;
-                sum2 = sum2 + a;
+                sum2 += a;
                 number = (number - a) / 10;
             }
-            int difference(sum1 - sum2);
 
-            if (difference == 0) {
+            if (sum1 == sum2) {
                 std::cout << "Congratulations!!!! Happy Ticket!!!!" << std::endl;
             }
             else
@@ -67,15 +74,14 @@ int main() {
         }
     }
 
-    else if (setNumber == 3) {
-        int number(0);
-        long long number2(0);
+    else if (setNumber == InverseNumbers) {
+        int number{};
+        long long number2{};
         std::cout << "Enter the number:" << std::endl;
         std::cin >> number;
         std::cout << number << std::endl;
         while (number != 0) {
-            int a(0);
-            a = number % 10;
+            int a = number % 10;
             number = (number - a) / 10;
             number2 = (number2 * 10) + a;
         }
@@ -87,18 +93,16 @@ int main() {
             std::cout << number2 << std::endl;
         }
     }
-    else if (setNumber == 4) {
+    else if (setNumber == SumOfOddElements) {
         constexpr int kMin{ 1 };
         constexpr int kMax{ 50 };
-        constexpr int kMin2{ -60 };
-        constexpr int kMax2{ 90 };
-        int sum(0);
-        int number(0);
-        int quantity(0);
-        std::cout
-            << "Enter a number from 1 to 50. So many numbers you will need to "
-            "enter later"
-            << std::endl;
+        constexpr int kMinValue{ -60 };
+        constexpr int kMaxValue{ 90 };
+        int sum{};
+        int number{};
+        int quantity{};
+        std::cout << "Enter a number from " << kMin << " to " << kMax
+            << ". So many numbers you will need to enter later" << std::endl;
         std::cin >> quantity;
         while (quantity > kMax || quantity < kMin) {
             std::cout << "Try again" << std::endl;
@@ -106,11 +110,11 @@ int main() {
         };
         int i(0);
         if (quantity < kMax && quantity > kMin) {
-            std::cout << " Enter " << quantity << " number from -60 to 90"
-                << std::endl;
+            std::cout << " Enter " << quantity << " number from " << kMinValue
+                << " to " << kMaxValue << std::endl;
             for (int i = 0; i < quantity; i++) {
                 std::cin >> number;
-                if (number > kMin2 && number < kMax2) {
+                if (number > kMinValue && number < kMaxValue) {
                     if (number % 2 != 0) {
 
                         sum = sum + number;
@@ -123,21 +127,21 @@ int main() {
         }
     }
 
-    else if (setNumber == 5) {
+    else if (setNumber == BestDivisor) {
         std::cout << "Enter a positive number" << std::endl;
-        int number(0);
+        int number{};
         std::cin >> number;
 
-        int best(0);
+        int best{};
 
         for (int divisor(1); divisor <= number; divisor++) {
 
             if ((number % divisor) == 0) {
-                int sum(0);
+                int sum{};
                 int b = divisor;
                 while (b != 0) {
                     int digit = b % 10;
-                    sum = sum + digit;
+                    sum += digit;
                     b = b / 10;
                 }
 
