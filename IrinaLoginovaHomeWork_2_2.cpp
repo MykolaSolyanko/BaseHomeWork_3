cfa704e0df29098dc4abfc0ef29122e2d04ef0fd
Loginova-irina <57185549+Loginova-irina@users.noreply.github.com>
2020-05-30 00:09:22 +0300

    Update IrinaLoginovaHomeWork_2_2.cpp

--- IrinaLoginovaHomeWork_2_2.cpp
+++ IrinaLoginovaHomeWork_2_2.cpp
@@ -2,28 +2,29 @@
 #include <stdexcept>
 
 int main() {
-    int NumberTicket{};
-    constexpr int MinRange = 1, MaxRange = 9;
-    std::cout << "Please enter 6 numbers of your ticket through Enter. Numbers "
-        "ranging from "
-        << MinRange << " to " << MaxRange << std::endl;
-    int sum_1 {};
-    int sum_2 {};
-    for (size_t i = 1; i < 4; ++i) {
-        std::cout << "Number is --> " << std::endl;
-        std::cin >> NumberTicket;
-        sum_1 += NumberTicket;
+  int NumberTicket{};
+  std::cout << "Please enter 6 numbers of your ticket." << std::endl;
+  std::cout << "Number is --> " << std::endl;
+  std::cin >> NumberTicket;
+  int x = NumberTicket % 10;
+  int sum_1{0};
+  int sum_2{0};
+  int i{0};
+  while (x != 0) {
+    i++;
+    if (i > 0 && i < 4) {
+      sum_1 += x;
     }
-    for (size_t i = 4; i < 7; ++i) {
-        std::cout << "Number is --> " << std::endl;
-        std::cin >> NumberTicket;
-        sum_2 += NumberTicket;
+    if (i > 3 && i < 7) {
+      sum_2 += x;
     }
-    if (sum_1 != sum_2) {
-        std::cout << "Congratulations! You NO have a lucky ticket." << std::endl;
-    }
-    else {
-        std::cout << "Congratulations! You have a lucky ticket." << std::endl;
-    }
-    return 0;
+    NumberTicket = (NumberTicket - x) / 10;
+    x = NumberTicket % 10;
+  }
+  if (sum_1 != sum_2) {
+    std::cout << "Congratulations! You NO have a lucky ticket." << std::endl;
+  } else {
+    std::cout << "Congratulations! You have a lucky ticket." << std::endl;
+  }
+  return 0;
 }
