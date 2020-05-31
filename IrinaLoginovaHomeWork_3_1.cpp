97c2190624aae715dd3acd794bf4e4931a0739f9
Loginova-irina <57185549+Loginova-irina@users.noreply.github.com>
2020-05-31 22:10:56 +0300

    Update file

--- IrinaLoginovaHomeWork_3_1.cpp
+++ IrinaLoginovaHomeWork_3_1.cpp
@@ -3,15 +3,23 @@
 #include <iostream>
 
 int main() {
-  constexpr int n = {150};
-  char ArrayString[n] = {};
-  std::cout << "Please enter your string no more than " << n << " characters."
-            << std::endl;
-  std::cout << "--->";
-  std::cin.getline(ArrayString, n);
-  int LengthArrayString = strlen(ArrayString);
-  for (int i = LengthArrayString; i >= 0; i--) {
-    std::cout << ArrayString[i];
-  }
-  return 0;
+    constexpr int SizeString{ 150 };
+    char ArrayString[SizeString]{};
+    std::cout << "Please enter your string no more than " << SizeString
+        << " characters." << std::endl;
+    std::cout << "--->";
+    std::cin.getline(ArrayString, SizeString);
+    int LengthArrayString = strlen(ArrayString);
+    int WholeHalf = LengthArrayString / 2;
+    int j = LengthArrayString - 1;
+    for (int i = 0; i <= WholeHalf; i++) {
+        char StartArray = ArrayString[i];
+        char EndArray = ArrayString[j];
+        ArrayString[i] = EndArray;
+        ArrayString[j] = StartArray;
+        j--;
+    }
+    std::cout << "Your string in reverse order" << std::endl;
+    std::cout << "---> " << ArrayString << std::endl;
+    return 0;
 }
